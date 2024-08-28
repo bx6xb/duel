5a90d8da7df357bb3ad2ac3961acf066dffa473b
Yan Turnt <bx6xbChosen1@gmail.com>
2024-08-28 17:35:50 +0500

    - added the ability for the hero to bounce off the mouse cursor

--- src/classes/Hero.ts
+++ src/classes/Hero.ts
@@ -5,9 +5,10 @@ export class Hero {
   heroStep = 1
   spellColor = "white"
   gap = 10
+  mousePos: ArrayXY = [0, 0]
   canvas: HTMLCanvasElement
   ctx: CanvasRenderingContext2D
-  rangeY: [number, number]
+  rangeY: ArrayXY
   x: number
   y: number
   isGoingDown: boolean
@@ -24,16 +25,11 @@ export class Hero {
     this.isGoingDown = Boolean(Math.round(Math.random())) // random boolean value
 
     this.draw()
-  }
-
-  getSpellColor() {
-    return this.spellColor
-  }
-  setSpellColor(spellColor: string) {
-    this.spellColor = spellColor
+    this.listen()
   }
 
   draw() {
+    // clears hero vertical line
     this.ctx.clearRect(
       this.x - this.heroRadius,
       0,
@@ -41,25 +37,69 @@ export class Hero {
       this.canvas.height
     )
 
+    // draws hero
     this.ctx.beginPath()
     this.ctx.fillStyle = "white"
     this.ctx.arc(this.x, this.y, this.heroRadius, 0, 2 * Math.PI)
     this.ctx.fill()
   }
-
   move() {
+    // checks for mouse collision
+    const startX = this.x - 20
+    const endX = this.x + 20
+    const startY = this.y - 20
+    const endY = this.y + 20
+
+    const mouseX = this.mousePos[0]
+    const mouseY = this.mousePos[1]
+
+    const betweenXAxis = startX <= mouseX && mouseX <= endX
+
+    if (betweenXAxis && startY <= mouseY && mouseY <= this.y) {
+      this.isGoingDown = true
+    } else if (betweenXAxis && this.y <= mouseY && mouseY <= endY) {
+      this.isGoingDown = false
+    }
+
+    // checks for edge collision
     if (this.y <= this.rangeY[0]) {
       this.isGoingDown = true
     } else if (this.y >= this.rangeY[1]) {
       this.isGoingDown = false
     }
 
+    // updates hero direction
     if (this.isGoingDown) {
       this.y += this.heroStep
     } else {
       this.y -= this.heroStep
     }
 
+    // redraw
     this.draw()
   }
+
+  getMousePosCallback() {
+    const setMousePos = (mousePos: ArrayXY) => {
+      this.mousePos = mousePos
+    }
+    return setMousePos.bind(this)
+  } // for subscribing to changes in mouse position on canvas
+
+  listen() {
+    // tracks mouse position
+    this.canvas.onmousemove = (e: MouseEvent) => {
+      this.mousePos = [e.offsetX, e.offsetY]
+    }
+  }
+
+  // data manipulation
+  getSpellColor() {
+    return this.spellColor
+  }
+  setSpellColor(spellColor: string) {
+    this.spellColor = spellColor
+  }
 }
+
+export type ArrayXY = [number, number]
--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,29 +1,49 @@
-import { useEffect, useRef } from "react"
+import { MouseEvent, useEffect, useRef, useState } from "react"
 import s from "./Canvas.module.scss"
-import { Hero } from "../../classes/Hero"
+import { ArrayXY, Hero } from "../../classes/Hero"
+
+type GetMousePosCallback = ([x, y]: ArrayXY) => void
 
 export const Canvas = () => {
-  const intervalsId = useRef<[number, number]>()
+  // local state
+  const [callbacks, setCallbacks] = useState<GetMousePosCallback[]>([])
 
+  // refs
+  const intervalsId = useRef<number[]>([])
   const cavnasRef = useRef<HTMLCanvasElement>(null)
 
   useEffect(() => {
-    const hero1 = new Hero(cavnasRef.current!, 20)
-    const hero2 = new Hero(cavnasRef.current!, 780)
-
-    intervalsId.current = [
-      setInterval(hero1.move.bind(hero1), 5),
-      setInterval(hero2.move.bind(hero2), 5),
-    ]
+    // creating heroes
+    ;[new Hero(cavnasRef.current!, 20), new Hero(cavnasRef.current!, 780)].map(
+      (hero) => {
+        setCallbacks((prev) => [...prev, hero.getMousePosCallback()]) // add callback to subscribe heroes on mouse position changes
+        intervalsId.current.push(setInterval(hero.move.bind(hero), 5)) // save interval id
+      }
+    )
 
     return () => {
       intervalsId.current!.map(clearInterval)
     }
   }, [])
 
+  const onMouseMove = (e: MouseEvent<HTMLCanvasElement>) => {
+    // get mouse position
+    const rect = e.currentTarget.getBoundingClientRect()
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+    const mousePos: ArrayXY = [x, y]
+
+    callbacks.map((f) => f(mousePos)) // subscribe heroes to changes in mouse position on canvas
+  }
+
   return (
     <div className={s.canvas}>
-      <canvas ref={cavnasRef} width={800} height={500} />
+      <canvas
+        ref={cavnasRef}
+        width={800}
+        height={500}
+        onMouseMove={onMouseMove}
+      />
     </div>
   )
 }
